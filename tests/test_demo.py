bbfb5f6196f546cc12300269e56a414019d2623e
Rory Meyer <rory.meyer@vliz.be>
2022-06-07 13:37:28 +0200

    getting ready for ver0

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,19 +1,17 @@
 import unittest
 from util4tests import run_single_test, log
+import os
 
-from pyodv import MyModel
+from pyodv import ODV_Struct
 
 
 class TestStringMethods(unittest.TestCase):
-
     # def test_upper(self):
     #     log.info("testing str.upper()")
     #     self.assertEqual('foo'.upper(), 'FOO')
-
     # def test_isupper(self):
     #     self.assertTrue('FOO'.isupper())
     #     self.assertFalse('Foo'.isupper())
-
     # def test_split(self):
     #     s = 'hello world'
     #     self.assertEqual(s.split(), ['hello', 'world'])
@@ -21,11 +19,24 @@ class TestStringMethods(unittest.TestCase):
     #     with self.assertRaises(TypeError):
     #         s.split(2)
 
-    def test_modelError(self):
-        m = MyModel()
-        self.assertAlmostEqual(m.my_method(10), 0.01)  # when comparing floats one should allow for some margin
-        with self.assertRaises(AssertionError):        # to trigger and test for intended failure
-            m.my_method(200)
+    def test_modelError(self): 
+        test_files = './test_files/'
+        all_valid = True
+        for filename in os.listdir(test_files ):
+            f = os.path.join(test_files, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                struct = None
+                isValid = False
+                try:
+                    struct = ODV_Struct(f)
+                    isValid = struct.valid_output()
+                    all_valid = all_valid and isValid
+                except Exception as err:
+                    log.warning(err)
+
+        self.assertTrue(all_valid)
+     
 
 
 if __name__ == "__main__":
